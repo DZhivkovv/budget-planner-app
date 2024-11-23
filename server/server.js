e766f0c7c4ca0457f63b3c42593d1995d9df737d
DZhivkovv <dobromirzhivkovv@gmail.com>
2024-11-23 10:47:04 +0200

    feat: Enhance Server Configuration
    - Setup CORS to allow requests from the frontend origin and enable
      credentials.
    - Add middleware to parse JSON bodies from incoming requests.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const app = express();
-// Import the MongoDB connection function
-const connectToDB = require('./config/db.js'); 
-// Load environment variables from .env file
+ // Load environment variables from .env file
 require('dotenv').config(); 
 
+const express = require('express');
+const app = express();
+
+// Use middleware to parse JSON bodies from incoming requests.
+app.use(express.json());
+
+const cors = require('cors');
+// Setup CORS to allow requests the frontend
+app.use(cors({
+    origin: process.env.FRONTEND_ORIGIN, // Explicitly allow your frontend origin
+    credentials: true, // Enable cookies or other credentials
+    }
+));
+
+// Import the MongoDB connection function
+const connectToDB = require('./config/db.js');
 // Start the server and listen on the specified port
 app.listen(process.env.PORT, () => {
     console.log("Server is running!");
